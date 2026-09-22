90111cc5f8cc799ee8460c095eb791f799aa5b61
d0nghee09 <ryudonghee@hanyang.ac.kr>
2026-09-22 11:27:24 +0900

    add mid, final boss enum

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -75,7 +75,14 @@ public final class DrawManager {
 		/** Bonus ship. */
 		EnemyShipSpecial,
 		/** Destroyed enemy ship. */
-		Explosion
+		Explosion,
+		
+		/* add enum MidBoss, FinalBoss  */
+		MidBoss_1,
+		MidBoss_2,
+		
+		FinalBoss_1,
+		FinalBoss_2
 	};
 
 	/**
